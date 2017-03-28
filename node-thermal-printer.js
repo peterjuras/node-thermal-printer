cc3fff449350066da2293b7b3a5cc11086d262d2
Peter Juras <p.juras@outlook.com>
2017-03-28 17:45:40 +0200

    Remove node-png dependency

--- node-thermal-printer.js
+++ node-thermal-printer.js
@@ -1,5 +1,5 @@
-var fs = require('fs'),
-    PNG = require('node-png').PNG;
+var fs = require('fs');
+    // PNG = require('node-png').PNG;
 
 var writeFile = require('write-file-queue')({
     retries : 1000, 						    // number of write attempts before failing
